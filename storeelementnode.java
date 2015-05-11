c98fe45edb32e9b203d0f4491589e2ffcf8e0ac2
Alicia Guerra <aiguerra@ucsd.edu>
2015-05-11 00:25:00 -0700

    Create storeelementnode.java

---
+++ storeelementnode.java
@@ -0,0 +1,6 @@
+/*
+Class implementing a node of a binary tree by storing references to an element, a parent node, a left node,
+and a right node.
+*/
+public class BTNode<E> implements BTPosition<E> {
+private E element; //element stored at this node
