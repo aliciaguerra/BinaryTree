8025f09cac3cc888c3918471421e937359897523
Alicia Guerra <aiguerra@ucsd.edu>
2015-05-11 00:21:38 -0700

    Update binarytreeinterface.java

--- binarytreeinterface.java
+++ binarytreeinterface.java
@@ -5,3 +5,11 @@ public interface BinaryTree<E> extends Tree<E> {
 /*Returns the left child of the node*/
 public Position<E> left(Position<E> v)
 throws InvalidPositionException, BoundaryViolationException;
+/*Returns the right child of the node*/
+public Position<E> right(Position<E> v)
+throws InvalidPositionException, BoundaryViolationException;
+/*Returns whether a node has a left child*/
+public boolean hasLeft(Position<E> v) throws InvalidPositionException;
+/*Returns whether a node has a right child*/
+public boolean hasRight(Position<E> v) throws InvalidPositionException;
+}
